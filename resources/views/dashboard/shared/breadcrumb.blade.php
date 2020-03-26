edfcddd8e1b1f8df4672bf00440a3f13201df582
Marc Hershey <marchershey@users.noreply.github.com>
2020-03-26 13:45:05 -0400

    update capitalization on breadcrumbs
    added class `text-capitalize` on breadcrumbs

--- resources/views/dashboard/shared/breadcrumb.blade.php
+++ resources/views/dashboard/shared/breadcrumb.blade.php
@@ -5,9 +5,9 @@
             @for($i = 1; $i <= count(Request::segments()); $i++)
                 <?php $segments .= '/'. Request::segment($i); ?>
                 @if($i < count(Request::segments()))
-                    <li class="breadcrumb-item">{{ Request::segment($i) }}</li>
+                    <li class="breadcrumb-item text-capitalize">{{ Request::segment($i) }}</li>
                 @else
-                    <li class="breadcrumb-item active">{{ Request::segment($i) }}</li>
+                    <li class="breadcrumb-item text-capitalize active">{{ Request::segment($i) }}</li>
                 @endif
             @endfor
           <!-- Breadcrumb Menu-->
