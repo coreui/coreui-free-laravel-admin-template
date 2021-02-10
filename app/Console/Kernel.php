af3b1edaacbb1dd8e45d22f9127f731d48f72a6f
Thor <thor@expluria.com>
2021-02-10 23:56:34 +0000

    Update Kernel.php
    fghfh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function scheggdule(Schedule $schedule)
     {
         // $schedule->command('inspire')
         //          ->hourly();
