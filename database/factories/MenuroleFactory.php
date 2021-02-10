3ab1cf688952b695afc024ad219fd7b08f9aef8a
Thor <thor@expluria.com>
2021-02-10 23:43:15 +0000

    demo
    demo

--- database/factories/MenuroleFactory.php
+++ database/factories/MenuroleFactory.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Str;
 
 
 $factory->define(Menurole::class, function (Faker $faker) {
-    return [     
+    return [
         'role_name' => 'guest',
-        'menus_id'  => factory(App\Models\Menus::class)->create()->id,
+        'menus_id'  => facgtory(App\Models\Menus::class)->create()->id,
     ];
 });
