cc71b8b0bccd777cdec3c7e6b305522d80abd199
MichaelPluz <michael@parkwhiz.com>
2021-05-15 17:45:19 +0500

    Fix potential issue in FreelyPositionedMenus
    Fix to get menu link with url() alike in MenuBuilder rather that with APP_URL

--- app/MenuBuilder/FreelyPositionedMenus.php
+++ app/MenuBuilder/FreelyPositionedMenus.php
@@ -13,7 +13,7 @@ class FreelyPositionedMenus{
             echo '<li class="' . $prefixClass . 'nav-item dropdown px-3">';
             echo '<a class="' . $prefixClass . 'nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="#">';
             if($data['hasIcon'] === true && $data['iconType'] === 'coreui'){
-                echo '<i class="' . $data['icon'] . ' ' . $prefixClass . 'nav-icon"></i>';    
+                echo '<i class="' . $data['icon'] . ' ' . $prefixClass . 'nav-icon"></i>';
             }
             echo $data['name'] . '</a>';
             echo '<div class="dropdown-menu">';
@@ -22,7 +22,7 @@ class FreelyPositionedMenus{
         }else{
             for($i = 0; $i < count($data); $i++){
                 if( $data[$i]['slug'] === 'link' ){
-                    echo '<a class="' . $prefixClass . 'nav-link dropdown-item" href="' . env('APP_URL', '') . $data[$i]['href'] . '">';
+                    echo '<a class="' . $prefixClass . 'nav-link dropdown-item" href="' . url($data[$i]['href']) . '">';
                     echo '<span class="' . $prefixClass . 'nav-icon"></span>' . $data[$i]['name'] . '</a>';
                 }elseif( $data[$i]['slug'] === 'dropdown' ){
                     self::renderDropdown( $data[$i], $prefixClass );
@@ -35,19 +35,19 @@ class FreelyPositionedMenus{
      * Render menu nav
      * @param  $data - array, result of GetSidebarMenu->get function
      * @param  $prefixClass - prefix to be placed before detailed classes
-     * @param  $navClass - class to be placed in nav 
+     * @param  $navClass - class to be placed in nav
      */
     public static function render($data, $prefixClass = '', $navClass = ''){
         echo '<ul class="' . $prefixClass . 'nav ' . $navClass . '">';
         foreach($data as $d){
             if($d['slug'] === 'link'){
                 echo '<li class="' . $prefixClass .'nav-item px-3">';
-                echo '<a class="' . $prefixClass . 'nav-link" href="' . env('APP_URL', '') . $d['href'] . '">';
+                echo '<a class="' . $prefixClass . 'nav-link" href="' . url( $d['href'] ) . '">';
                 if($d['hasIcon'] === true){
                     if($d['iconType'] === 'coreui'){
                         echo '<i class="' . $d['icon'] . ' ' . $prefixClass . 'nav-icon"></i>';
                     }
-                } 
+                }
                 echo $d['name'];
                 echo '</a>';
                 echo '</li>';
@@ -59,7 +59,7 @@ class FreelyPositionedMenus{
                     if($d['iconType'] === 'coreui'){
                         echo '<i class="' . $d['icon'] . ' ' . $prefixClass . 'nav-icon"></i>';
                     }
-                } 
+                }
                 echo $d['name'];
                 echo '</li>';
             }
