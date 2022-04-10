e453a1666166069ff272b41b97e8d8e3b92d7b8e
Ida Bagus Redy Santiawan <goesredy@gmail.com>
2022-04-10 08:27:05 +0800

    Fix "curly" braces not being closed
    Update navbar-toggler "curly" braces not being closed

--- resources/views/dashboard/shared/header.blade.php
+++ resources/views/dashboard/shared/header.blade.php
@@ -2,7 +2,7 @@
       
     <div class="c-wrapper">
       <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-        <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="#"><img class="c-header-brand" src="{{ url('/assets/brand/coreui-base.svg" width="97" height="46" alt="CoreUI Logo"></a>
+        <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="#"><img class="c-header-brand" src="{{ url('/assets/brand/coreui-base.svg') }}" width="97" height="46" alt="CoreUI Logo"></a>
         <button class="c-header-toggler c-class-toggler ml-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true"><span class="c-header-toggler-icon"></span></button>
         <?php
             use App\MenuBuilder\FreelyPositionedMenus;
